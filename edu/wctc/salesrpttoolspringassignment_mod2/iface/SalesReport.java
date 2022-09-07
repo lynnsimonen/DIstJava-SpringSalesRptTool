9b56c89046538f526900acba308ef9adba7562be
Lynn Simonen <lynnsimonen@yahoo.com>
2022-09-07 16:42:35 -0500

    deleted some files after moving files from laptop to github

--- edu/wctc/salesrpttoolspringassignment_mod2/iface/SalesReport.java
+++
@@ -1,9 +0,0 @@
-package edu.wctc.salesrpttoolspringassignment_mod2.iface;
-
-import edu.wctc.salesrpttoolspringassignment_mod2.Sale;
-
-import java.util.List;
-
-public interface SalesReport {
-    void displayReport(List<Sale> sales);
-}
